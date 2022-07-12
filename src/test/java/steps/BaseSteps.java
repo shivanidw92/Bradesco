bfcdd5339114f954c766f71e46b6244f580e2df3
I418455 <I418455>
2022-07-12 09:22:11 -0300

    remove dependencia relatorios

--- src/test/java/steps/BaseSteps.java
+++ src/test/java/steps/BaseSteps.java
@@ -5,7 +5,7 @@ import cucumber.api.java.After;
 import cucumber.api.java.AfterStep;
 import cucumber.api.java.Before;
 import org.openqa.selenium.WebDriver;
-import report.Screenshot;
+//import report.Screenshot;
 import utils.GlobalDriver;
 
 public class BaseSteps {
@@ -20,14 +20,14 @@ public class BaseSteps {
 
     @After
     public static void finish(Scenario scenario) {
-        scenario.embed(Screenshot.getByteScreenshot(driver), "image/png");
+//        scenario.embed(Screenshot.getByteScreenshot(driver), "image/png");
 //        scenario.attach(Screenshot.getByteScreenshot(driver), "image/png", scenario.getName());
         GlobalDriver.close();
     }
 
     @AfterStep
     public static void afterStep(Scenario scenario) {
-        scenario.embed(Screenshot.getByteScreenshot(driver), "image/png");
+//        scenario.embed(Screenshot.getByteScreenshot(driver), "image/png");
 //        scenario.attach(Screenshot.getByteScreenshot(driver), "image/png", "");
     }
 
