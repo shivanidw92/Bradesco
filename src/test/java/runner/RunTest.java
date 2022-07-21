72101751945b3524467eba669d3dd9f0686babf7
I418455 <I418455>
2022-07-21 14:55:14 -0300

    Add dryrun

--- src/test/java/runner/RunTest.java
+++ src/test/java/runner/RunTest.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
         features = "./src/test/resources/features",
         glue = {"steps"},
         tags = "@run2",
-        dryRun = true,
+//        dryRun = true,
         monochrome = true,
         plugin = {
 //            "pretty",
