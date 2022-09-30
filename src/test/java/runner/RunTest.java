e108884593516ce86e231711aeaa1ab0ce6786d0
I418455 <I418455>
2022-09-30 11:33:37 -0300

    Merge with master

--- src/test/java/runner/RunTest.java
+++ src/test/java/runner/RunTest.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "./src/test/resources/features",
         glue = {"steps"},
-        tags = "@runP",
+        tags = "@run2",
         monochrome = true,
         plugin = {
             "pretty",
